1db5b998c2021b79919e11db1d04140a3f22325a
dakotash <dakotash@email.sc.edu>
2025-06-02 18:49:52 -0400

    updated json

---
+++ csce242/Projects/part6/js/trainers.js
@@ -0,0 +1,10 @@
+const getShoes = async() => {
+    const url = "https://portiaportia.github.io/json/shoes.json";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    }catch(error){
+        console.log(error);
+    }
+};
